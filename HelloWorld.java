657089549ca1880ecea35bd8d550ff57f7d5e6e1
nayan0210git config --global user.email nayanpandey2001@gmail.com <nayanpandey2001@gmail.com>
2024-07-19 23:23:54 +0530

    final changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,11 @@ public class HelloWorld {
     public static void main(String[] args){
         Scanner sc  =new Scanner (System.in);
         System.out.println("Enter number");
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int sum=a+b;
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int sum=c+d;
         System.out.println(sum);
+        System.out.println("hiii helloo");
+
     }
 }
