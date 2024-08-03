53abad3fdd40e90a5bd383796a58854988ce7d8a
nayan0210git config --global user.email nayanpandey2001@gmail.com <nayanpandey2001@gmail.com>
2024-08-03 19:58:42 +0530

    commit

--- NumberPattern5.java
+++ NumberPattern5.java
@@ -8,7 +8,7 @@
 
 public class NumberPattern5 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc =new Scanner(System.in);
         System.out.println("Enter the number");
         int n=sc.nextInt();
 
